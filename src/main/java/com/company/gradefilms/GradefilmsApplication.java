855c1d3b15ffb3e0babe1311e3d530e36db30289
Vitaliy Babich <v.babich.225@gmail.com>
2022-08-19 11:31:54 +0300

    Create project

---
+++ src/main/java/com/company/gradefilms/GradefilmsApplication.java
@@ -0,0 +1,13 @@
+package com.company.gradefilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradefilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradefilmsApplication.class, args);
+	}
+
+}
